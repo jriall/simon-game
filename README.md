1cdd56f4816adb90c8cc58d5f425023d3d42e65a
JamesRiall <jamesrariall@gmail.com>
2017-11-14 08:15:06 +0000

    amended readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Simon Game
 
+http://jamesriall.co.uk/simon-game/
+
+## The Project
+
 A web replication of the classic game of memory - Simon! Built with HTML, CSS, and Javascript using jQuery as part of the freeCodeCamp Front End Curriculum.
 
 After turning the game on and starting, users are displayed a sequence of tones and lights, with the game requiring the player to repeate the series to progress to the next level. Levels increase the sequence by one step each round, and the tempo with which the game displays the sequence increases halfway throught the game.
@@ -7,3 +11,5 @@ After turning the game on and starting, users are displayed a sequence of tones 
 Users have the ability to reduce or increase the level at which the game is completed (default is 20), and the ability to turn 'Strict Mode' on and off (an incorrect selection with strict mode on results in a game reset, with strict mode off, users only have to restart the level).
 
 Planned feature additions - ability to select difficulty (i.e. increase or decrease the tempo of the sequence the game displays to the user each round), and a timer which resets the game if the user takes too long to make a selection.
+
+![ScreenShot](http://res.cloudinary.com/jamesriall/image/upload/v1469029646/simon_mock_up_juwvn1.png)
--- js/index.js
+++ js/index.js
@@ -8,180 +8,180 @@ let sequenceRunning = false;
 let tempo = 1000;
 let gameLength = 20;
 
-//load an array for a new game - random selections of red, blue, yellow and green.
+// load an array for a new game - random selections of red, blue, yellow and green.
 function setSequence() {
-    let color = "";
-    for (let i = 0; i < gameLength; i++) {
-        //change back to 20 when ready
-        color = Math.floor(Math.random() * 4 + 1);
-        switch (color) {
-            case 1:
-                sequence.push("red");
-                break;
-            case 2:
-                sequence.push("blue");
-                break;
-            case 3:
-                sequence.push("yellow");
-                break;
-            case 4:
-                sequence.push("green");
-                break;
-        }
+  let color = '';
+  for (let i = 0; i < gameLength; i++) {
+    // change back to 20 when ready
+    color = Math.floor(Math.random() * 4 + 1);
+    switch (color) {
+      case 1:
+        sequence.push('red');
+        break;
+      case 2:
+        sequence.push('blue');
+        break;
+      case 3:
+        sequence.push('yellow');
+        break;
+      case 4:
+        sequence.push('green');
+        break;
     }
+  }
 }
 
-//function to load sound effects
+// function to load sound effects
 function playSound(path) {
-    let audioElement = document.createElement("audio");
-    audioElement.setAttribute("src", path);
-    audioElement.play();
+  const audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', path);
+  audioElement.play();
 }
 
-//reset game
+// reset game
 function reset() {
-    gameRunning = false;
-    currentRound = 0;
-    clicksInRound = [];
-    $("#round").html("--");
-    tempo = 1000;
+  gameRunning = false;
+  currentRound = 0;
+  clicksInRound = [];
+  $('#round').html('--');
+  tempo = 1000;
 }
 
-//handle turning the game on and off
-$(".on-off-button").click(function() {
-    if (this.id == "on-button" && gameSwitchedOn === false) {
-        gameSwitchedOn = true;
-        $("#on-button").css("background-color", "blue");
-        $("#off-button").css("background-color", "black");
-    } else if (this.id == "off-button" && gameSwitchedOn === true) {
-        gameSwitchedOn = false;
-        $("#off-button").css("background-color", "blue");
-        $("#on-button").css("background-color", "black");
-        reset();
-    }
+// handle turning the game on and off
+$('.on-off-button').click(function () {
+  if (this.id == 'on-button' && gameSwitchedOn === false) {
+    gameSwitchedOn = true;
+    $('#on-button').css('background-color', 'blue');
+    $('#off-button').css('background-color', 'black');
+  } else if (this.id == 'off-button' && gameSwitchedOn === true) {
+    gameSwitchedOn = false;
+    $('#off-button').css('background-color', 'blue');
+    $('#on-button').css('background-color', 'black');
+    reset();
+  }
 });
 
-//handle clicking the start/reset button
-$("#start-reset").click(function() {
-    if (gameSwitchedOn) {
-        if (!gameRunning) {
-            gameRunning = true;
-            sequence = [];
-            setSequence();
-            currentRound = 1;
-            changeRound();
-        } else if (gameRunning) {
-            reset();
-        }
-    }
-});
-
-//strict button - change strict on or off. Only allow functionality if game is not in progress.
-$("#strict").click(function() {
+// handle clicking the start/reset button
+$('#start-reset').click(() => {
+  if (gameSwitchedOn) {
     if (!gameRunning) {
-        if (!strict) {
-            strict = true;
-            $("#strict-light").css("background-color", "red");
-        } else {
-            strict = false;
-            $("#strict-light").css("background-color", "black");
-        }
+      gameRunning = true;
+      sequence = [];
+      setSequence();
+      currentRound = 1;
+      changeRound();
+    } else if (gameRunning) {
+      reset();
     }
+  }
 });
 
-//handle player winning the game
+// strict button - change strict on or off. Only allow functionality if game is not in progress.
+$('#strict').click(() => {
+  if (!gameRunning) {
+    if (!strict) {
+      strict = true;
+      $('#strict-light').css('background-color', 'red');
+    } else {
+      strict = false;
+      $('#strict-light').css('background-color', 'black');
+    }
+  }
+});
+
+// handle player winning the game
 function handleWin() {
-    playSound("sounds/win.mp3");
-    $("#round").html("W");
-    $(".game-button").addClass("highlight");
-    setTimeout(function() {
-        $(".game-button").removeClass("highlight");
-    }, 350);
-    setTimeout(function() {
-        reset();
-    }, 3000);
+  playSound('sounds/win.mp3');
+  $('#round').html('W');
+  $('.game-button').addClass('highlight');
+  setTimeout(() => {
+    $('.game-button').removeClass('highlight');
+  }, 350);
+  setTimeout(() => {
+    reset();
+  }, 3000);
 }
 
-//handle player completing a round
+// handle player completing a round
 function changeRound() {
-    $("#round").html(currentRound);
-    clicksInRound = [];
-    if (currentRound === Math.floor(gameLength / 2)) {
-        tempo = 750;
-    }
-    displayRoundSequence();
-    gameRunning = true;
+  $('#round').html(currentRound);
+  clicksInRound = [];
+  if (currentRound === Math.floor(gameLength / 2)) {
+    tempo = 750;
+  }
+  displayRoundSequence();
+  gameRunning = true;
 }
 
-//handle player making a mistake - restart round or restart game (if strict mode is on)
+// handle player making a mistake - restart round or restart game (if strict mode is on)
 function handleWrong() {
-    playSound("sounds/mistake.mp3");
-    $(".game-button").addClass("highlight");
-    setTimeout(function() {
-        $(".game-button").removeClass("highlight");
-    }, 350);
-    if (strict) {
-        //game reset
-        reset();
-    } else if (!strict) {
-        //restart round
-        currentRound--;
-        changeRound();
-    }
+  playSound('sounds/mistake.mp3');
+  $('.game-button').addClass('highlight');
+  setTimeout(() => {
+    $('.game-button').removeClass('highlight');
+  }, 350);
+  if (strict) {
+    // game reset
+    reset();
+  } else if (!strict) {
+    // restart round
+    currentRound--;
+    changeRound();
+  }
 }
 
-//highlight a color, play sound, and remove highlight for when player clicks or sequence is displaying.
-let playColor = function(color) {
-    $("#" + color).addClass("highlight");
-    playSound("sounds/" + color + "-tone.mp3");
-    setTimeout(function() {
-        $("#" + color).removeClass("highlight");
-    }, 350);
+// highlight a color, play sound, and remove highlight for when player clicks or sequence is displaying.
+const playColor = function (color) {
+  $(`#${color}`).addClass('highlight');
+  playSound(`sounds/${color}-tone.mp3`);
+  setTimeout(() => {
+    $(`#${color}`).removeClass('highlight');
+  }, 350);
 };
 
-//generate the current round sequence and display to the player
-let displayRoundSequence = function() {
-    sequenceRunning = true;
-    let unfreeze = setTimeout(function() {
-        sequenceRunning = false;
-    }, 1000 * currentRound);
-    $(".on-off-button, #start-reset").click(function() {
-        clearTimeout(unfreeze);
+// generate the current round sequence and display to the player
+let displayRoundSequence = function () {
+  sequenceRunning = true;
+  const unfreeze = setTimeout(() => {
+    sequenceRunning = false;
+  }, 1000 * currentRound);
+  $('.on-off-button, #start-reset').click(() => {
+    clearTimeout(unfreeze);
+  });
+  for (let k = 0; k < currentRound; k++) {
+    const testColor = setTimeout(() => {
+      playColor(sequence[k]);
+    }, tempo * k + 1500);
+    $('.on-off-button, #start-reset').click(() => {
+      clearTimeout(testColor);
     });
-    for (let k = 0; k < currentRound; k++) {
-        let testColor = setTimeout(function() {
-            playColor(sequence[k]);
-        }, tempo * k + 1500);
-        $(".on-off-button, #start-reset").click(function() {
-            clearTimeout(testColor);
-        });
-    }
+  }
 };
 
-//handle player clicking a color button, checking if correct or wrong and where player is in the round.
-$(".game-button").click(function() {
-    if (gameRunning && gameSwitchedOn && !sequenceRunning) {
-        clicksInRound.push(this.id);
-        for (let j = 0; j < clicksInRound.length; j++) {
-            if (clicksInRound[j] !== sequence[j]) {
-                handleWrong();
-                break;
-            }
-            playColor(this.id);
-        }
-        if (clicksInRound.length === currentRound) {
-            //round completed, new round
-            currentRound++;
-            if (currentRound === gameLength + 1) {
-                return handleWin();
-            }
-            changeRound();
-        }
+// handle player clicking a color button, checking if correct or wrong and where player is in the round.
+$('.game-button').click(function () {
+  if (gameRunning && gameSwitchedOn && !sequenceRunning) {
+    clicksInRound.push(this.id);
+    for (let j = 0; j < clicksInRound.length; j++) {
+      if (clicksInRound[j] !== sequence[j]) {
+        handleWrong();
+        break;
+      }
+      playColor(this.id);
     }
+    if (clicksInRound.length === currentRound) {
+      // round completed, new round
+      currentRound++;
+      if (currentRound === gameLength + 1) {
+        return handleWin();
+      }
+      changeRound();
+    }
+  }
 });
 
-//handle the change level input
-$("#level-change").submit(function(event) {
-    event.preventDefault();
-    gameLength = parseInt(this.elements[0].value);
+// handle the change level input
+$('#level-change').submit(function (event) {
+  event.preventDefault();
+  gameLength = parseInt(this.elements[0].value);
 });
